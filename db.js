beb0b377de32982486a673eead5a8d4c745d2450
edg1000 <andreedghill5@gmail.com>
2026-03-31 20:53:17 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
+const sqlite3 = require('sqlite3').verbose();
+const { open } = require('sqlite');
+
+async function initDB() {
+  return open({
+    filename: './safevault.db',
+    driver: sqlite3.Database
+  });
+}
+
+module.exports = initDB;
 
