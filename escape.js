17b2ca7509e7a38294b75135dbff026441b3f6b7
edg1000 <andreedghill5@gmail.com>
2026-03-31 20:44:44 -0400

    Update escape.js

--- escape.js
+++ escape.js
@@ -1,1 +1,7 @@
+function escapeHTML(str) {
+  return str.replace(/[&<>"']/g, (char) => ({
+    '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'
+  }[char]));
+}
+module.exports = { escapeHTML };
 
