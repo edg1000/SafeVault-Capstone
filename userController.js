3b1f84e6c08cb2c3ba6bf8bf9cca49e610c2a889
edg1000 <andreedghill5@gmail.com>
2026-03-31 20:37:02 -0400

    Update userController.js

--- userController.js
+++ userController.js
@@ -1,1 +1,12 @@
+// src/userController.js
+const db = require('./db');
+const { validateUsername } = require('./inputValidation');
+
+async function getUser(username) {
+  if (!validateUsername(username)) {
+    throw new Error("Invalid username format");
+  }
+  const stmt = await db.prepare("SELECT * FROM users WHERE username = ?");
+  return await stmt.get(username); // parameterized query prevents SQL injection
+}
 
