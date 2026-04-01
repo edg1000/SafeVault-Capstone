5a6d7067ae3a9d9b05a60e13df27680c406dfe1d
edg1000 <andreedghill5@gmail.com>
2026-03-31 20:46:26 -0400

    Update security.test.js

--- security.test.js
+++ security.test.js
@@ -1,1 +1,10 @@
+const { validateUsername } = require('../src/inputValidation');
+
+test("rejects SQL injection attempt", () => {
+  expect(validateUsername("admin' OR 1=1 --")).toBe(false);
+});
+
+test("accepts valid username", () => {
+  expect(validateUsername("validUser123")).toBe(true);
+});
 
